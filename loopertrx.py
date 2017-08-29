18f8c32a4d2d39b0fd154edeaa10990dd7913408
Reiner Herrmann <reiner@reiner-h.de>
2017-08-29 15:54:11 +0200

    Add license file and copyright header (GPL2+)

--- loopertrx.py
+++ loopertrx.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+#
+# loopertrx: import/export audio data from some looper pedals.
+#
+# Copyright (C) 2017  Reiner Herrmann <reiner@reiner-h.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 import random
 import struct
